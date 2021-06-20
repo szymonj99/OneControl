d8584230b37fd354fd11ffcb5504c14dd902f4ea
szymonj99 <szymon.j@hotmail.com>
2021-06-20 17:52:09 +0100

    Remove submodule, add CMake visual studio project

--- OneControl/OneControl.cpp
+++ OneControl/OneControl.cpp
@@ -1,14 +1,10 @@
-﻿// OneControl.cpp : Defines the entry point for the application.
-//
-
-#define ASIO_STANDALONE
+﻿#define ASIO_STANDALONE
 
 #include "OneControl.h"
 
-using namespace std;
-
 int main()
 {
-	cout << "Hello CMake." << endl;
+	std::cout << "Hello CMake." << std::endl;
+	std::cin.get();
 	return 0;
 }
