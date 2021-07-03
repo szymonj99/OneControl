7255c2394ac95aae055f3429a7ebfb31d2df31b4
szymonj99 <szymon.j@hotmail.com>
2021-07-03 15:56:17 +0100

    Add initial mouse code

--- OneControl/IMouse.cpp
+++ OneControl/IMouse.cpp
@@ -1,1 +1,11 @@
 #include "IMouse.h"
+
+void oc::IMouse::MoveMouseTo(const int32_t x, const int32_t y)
+{
+	m_pMouseInterface->MoveMouseTo(x, y);
+}
+
+std::pair<int32_t, int32_t> oc::IMouse::GetMousePosition()
+{
+	return m_pMouseInterface->GetMousePosition();
+}
