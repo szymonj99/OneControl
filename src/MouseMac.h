73176d7dfdf8e73250e8cafd0c48182f694660ed
Szymon J <32754152+szymonj99@users.noreply.github.com>
2021-08-17 01:38:40 +0000

    Add GitHub Actions for CMake

--- src/MouseMac.h
+++ src/MouseMac.h
@@ -2,13 +2,18 @@
 
 #ifdef __APPLE__
 
+#include <cstdint>
+#include <utility>
+
 #include "MouseMac.h"
 
 namespace oc
 {
 	class MouseMac
 	{
-
+	public:
+		void MoveMouseTo(const int32_t x, const int32_t y);
+		std::pair<int32_t, int32_t> GetMousePosition();
 	};
 }
 
